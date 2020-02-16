0d7e9f32ddf2c1207d96ca66cf1e05ba6d7bd04c
Vitaly Lavrov <lavrovvv@gmail.com>
2020-02-16 01:56:11 +0100

    src 0.3.1 for linux

--- src-linux.rb
+++ src-linux.rb
@@ -1,9 +1,9 @@
 class SrcLinux < Formula
   desc "Git assistant"
   homepage ""
-  version "0.3.0"
+  version "0.3.1"
   url "https://github.com/lavrov/src/releases/download/v#{version}/src-linux-amd64"
-  sha256 "12ff15edbf4513135331d7468a1bb2717be9451bf3528aadeec17a1b6a5fbc95"
+  sha256 "8e99e4177578b03f831203445e431db34d52cd6bf9a9d2250616d7e1ae4e9001"
 
   def install
     libexec.install Dir["*"]
