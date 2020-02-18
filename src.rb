79a86c98b5abb5d6e91657dfd571225802372ef8
Vitaly Lavrov <lavrovvv@gmail.com>
2020-02-18 17:17:41 +0100

    src 0.4.0 for macos

--- src.rb
+++ src.rb
@@ -1,9 +1,9 @@
 class Src < Formula
   desc "Git assistant"
   homepage ""
-  version "0.3.0"
+  version "0.4.0"
   url "https://github.com/lavrov/src/releases/download/v#{version}/src-darwin-amd64"
-  sha256 "d835d235e70affb8a4c1437b313d2aef0bc31656aedd968ba8b08a96709ab1a5"
+  sha256 "94961007a244f42e236f9a2abbbc4a325ebed1eb683eec155b357533f97e0356"
 
   def install
     libexec.install Dir["*"]
